6d2003afaec5c90cf2fd5eaf6fbaae8c9fa231e3
amjadkhansarwar <muhammad.amjad@iths.se>
2022-04-25 23:23:57 +0200

    -Explantion of code

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -9,20 +9,22 @@ use Exception;
 
 class ProductsController extends Controller
 {
-    //
-   
+    
 
     function search(){
         $skuList = request('sku', []);
         $products = [];
         foreach ($skuList as $sku) {
+            // Check error
             if ($sku==501) {
                 throw new Error("Internal Server Error", 501);
             }
+            // wait function
             if ($sku==98765) {
                 $sec = rand(5, 10);
                 sleep($sec);
             }
+            // check value if availble on cache
             if (!cache()->has($sku)) {
                 $price = rand(300, 1200);
                 $vat = round($price*25/100, 2);
